0006cea16941224c9c0381933f3ffa8c768d813d
r-serori <155217702+r-serori@users.noreply.github.com>
2024-10-25 02:04:52 +0000

    sec com

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -4,9 +4,11 @@ function Counter() {
   const [count, setCount] = useState(0);
 
   return (
-    <div>
+    <div className="container">
       <p>現在のカウント: {count}</p>
       <button onClick={() => setCount(count + 1)}>増やす</button>
     </div>
   );
 }
+
+export default Counter;
--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Counter from "./Counter";
+import "./styles.css";
 
 const rootElement = document.getElementById("root");
 const root = ReactDOM.createRoot(rootElement);
